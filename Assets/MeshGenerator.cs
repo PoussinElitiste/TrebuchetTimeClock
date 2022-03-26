fb34694fb87ab504ccc1418ee600bec8074cfa22
Khalil Hajlaoui <khalil.hajlaoui@gmail.com>
2022-03-26 01:47:37 -0400

    [Tool] generate cube and tetrahedron mesh size

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -6,6 +6,7 @@ namespace Game.Data
 {
     public static class CubeMeshData
     {
+        public const float scaleAdjust = 0.5f; // vertices coordinate made Cube twice size
         public static Vector3[] vertices =
         {
             // face Z positive
@@ -32,11 +33,17 @@ namespace Game.Data
             new int[] { 3, 2, 7, 6}  // 5: Face Dwn
         };
     
-        public static Vector3[] faceVertices(int dir, float scale, Vector3 pos)
+        public static Vector3[] faceVertices(int dir, Vector3 size, Vector3 pos)
         {
             Vector3[] fv = new Vector3[4];
             for (int i = 0; i < fv.Length; ++i)
-                fv[i] = (vertices[faceTriangles[dir][i]] * scale) + pos;
+            {
+                Vector3 v = vertices[faceTriangles[dir][i]];
+                fv[i].x = v.x * size.x;
+                fv[i].y = v.y * size.y;
+                fv[i].z = v.z * size.z;
+                fv[i] += pos;
+            }
 
             return fv;
         }
@@ -48,25 +55,30 @@ namespace Game.Data
         {
             // face Z positive
             new Vector3( 0, 0, 0), // 0
-            new Vector3( 0, 1, 0), // y
             new Vector3( 0, 0, 1), // z
+            new Vector3( 0, 1, 0), // y
             new Vector3( 1, 0, 0), // x
         };
 
-        public static int[][] faceTriangles =
+        public static int[] faceTriangles =
         {
             // parcours en sens horlogique
-            new int[] { 0, 1, 2}, // 0: Face 0-z-y
-            new int[] { 0, 1, 3}, // 1: Face 0-z-x
-            new int[] { 2, 3, 1}, // 2: Face 0-y-x
-            new int[] { 1, 2, 3}, // 3: Face x-y-z
+            0, 1, 2, // 0: Face 0-Z-Y
+            0, 2, 3, // 1: Face 0-Y-X
+            0, 3, 1, // 2: Face 0-X-Z
+            3, 2, 1  // 3: Face X-Y-Z
         };
 
-        public static Vector3[] faceVertices(int dir, float scale, Vector3 pos)
+        public static Vector3[] faceVertices(Vector3 size, Vector3 pos)
         {
-            Vector3[] fv = new Vector3[3];
+            Vector3[] fv = new Vector3[vertices.Length];
             for (int i = 0; i < fv.Length; ++i)
-                fv[i] = (vertices[faceTriangles[dir][i]] * scale) + pos;
+            {
+                fv[i].x = vertices[i].x * size.x;
+                fv[i].y = vertices[i].y * size.y;
+                fv[i].z = vertices[i].z * size.z;
+                fv[i] += pos;
+            }
 
             return fv;
         }
@@ -80,33 +92,32 @@ public class MeshGenerator : MonoBehaviour
     List<Vector3> vertices;
     List<int> triangles;
 
-    public float scale = 1f;
-    private float adjScale = 1f;
-    public int posX, posY, posZ;
+    public Vector3 position;
+    public Vector3 size = new Vector3(1f,1f,1f);
 
     void Start()
     {
         mesh = GetComponent<MeshFilter>().mesh;
-        adjScale = scale * 0.5f;
 
-        //MakeCube(adjScale, new Vector3(posX*adjScale, posY * adjScale, posZ * adjScale));
-        MakeTetrahedron(adjScale, new Vector3(posX*adjScale, posY * adjScale, posZ * adjScale));
+        MakeCube(size, position);
+        //MakeTetrahedron(size, position);
+        //CreateShape();
         UpdateMesh();
     }
 
-    void MakeCube(float cubeScale, Vector3 cubPos)
+    void MakeCube(Vector3 size, Vector3 pos)
     {
         vertices = new List<Vector3>();
         triangles = new List<int>();
         for (int i = 0; i < 6; i++)
         {
-            MakeQuadFace(i, cubeScale, cubPos);
+            MakeQuadFace(i, size * CubeMeshData.scaleAdjust, pos);
         }
     }
 
-    void MakeQuadFace(int dir, float faceScale, Vector3 facePos)
+    void MakeQuadFace(int dir, Vector3 size, Vector3 pos)
     {
-        vertices.AddRange(CubeMeshData.faceVertices(dir, faceScale, facePos));
+        vertices.AddRange(CubeMeshData.faceVertices(dir, size, pos));
         int vCount = vertices.Count;
         // first triangle quad
         triangles.Add(vCount - 4 + 0);
@@ -118,20 +129,32 @@ public class MeshGenerator : MonoBehaviour
         triangles.Add(vCount - 4 + 3);
     }
 
-    void MakeTetrahedron(float tetraScale, Vector3 cubPos)
+    void MakeTetrahedron(Vector3 size, Vector3 pos)
     {
         vertices = new List<Vector3>();
         triangles = new List<int>();
-        for (int i = 0; i < 4; ++i)
-        {
-            MakeTriangleFace(i, tetraScale, cubPos);
-        }
+        vertices.AddRange(TetraMeshData.faceVertices(size, pos));
+        //vertices.AddRange(TetraMeshData.vertices);
+        triangles.AddRange(TetraMeshData.faceTriangles);
     }
 
-    void MakeTriangleFace(int dir, float faceScale, Vector3 facePos)
+    private void CreateShape()
     {
-        vertices.AddRange(TetraMeshData.faceVertices(dir, faceScale, facePos));
-        triangles.AddRange(TetraMeshData.faceTriangles[dir]);
+        vertices = new List<Vector3>()
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(0,1,0),
+            new Vector3(1,0,0)
+        };
+
+        triangles = new List<int>()
+        {
+            0, 1, 2, // 0-Z-Y
+            0, 2, 3, // 0-Y-X
+            0, 3, 1, // 0-X-Z
+            3, 2, 1  // X-Y-Z
+        };
     }
 
     private void UpdateMesh()
