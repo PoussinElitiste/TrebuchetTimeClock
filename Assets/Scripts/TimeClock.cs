352821feae6c1043851bd47c16fc5e85b74bf0df
Khalil Hajlaoui <khalil.hajlaoui@gmail.com>
2022-03-27 15:20:31 -0400

    [Game] add clockwork model type

--- Assets/Scripts/TimeClock.cs
+++ Assets/Scripts/TimeClock.cs
@@ -1,19 +1,51 @@
-using System;
-
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 namespace Game.Run
 {
+    // ALIGNED WITH CLOCK HAND MODELS!
+    enum HandleType
+    {
+        Cube = 0,
+        Cylinder,
+        Tetra,
+        Triangle
+    }
+
     public class TimeClock : MonoBehaviour
     {
+        [System.Serializable]
+        struct ClockHandModel
+        {
+            public Mesh minute;
+            public Mesh hour;
+        }
+
         [SerializeField]
         private Transform minuteHand;
+        private MeshFilter minuteHandMesh;
+
         [SerializeField]
         private Transform hourHand;
+        private MeshFilter hourHandMesh;
 
         [SerializeField]
         private TextMeshPro timeText;
+
+        [SerializeField]
+        private HandleType defaultClockModel = HandleType.Cube;
+        [SerializeField]
+        private List<ClockHandModel> clockHandModels = new ();
+
+        private void Start()
+        {
+            minuteHandMesh = minuteHand.GetComponentInChildren<MeshFilter>();
+            hourHandMesh = hourHand.GetComponentInChildren<MeshFilter>();
+
+            minuteHandMesh.sharedMesh = clockHandModels[(int)defaultClockModel].minute;
+            hourHandMesh.sharedMesh = clockHandModels[(int)defaultClockModel].hour;
+        }
 
         public void SetCurrentTime(int minuteAngle, float hourAngle, string display)
         {
